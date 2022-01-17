073339b620244521f30f639a24526e50d5711423
Tomas <tomasignaciocontreras@hotmail.com>
2022-01-16 21:24:31 -0300

    checkout product front, basket logic added, added login frontend and functionalities

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./Login.css"
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+        // firebase login stuff
+    }
+
+    const register = (e) => {
+        e.preventDefault();
+        // firebase register stuff
+    }
+
     return (
         <div className="login">
-            <Link to="/"> 
-            <img  className="login__logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/800px-Amazon_logo.svg.png" alt="" />
+            <Link to="/">
+                <img className="login__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/800px-Amazon_logo.svg.png" alt="" />
             </Link>
 
             <div className="login__container">
                 <h1>Sign in</h1>
                 <form action="">
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
 
-                    <button className="login__signInButton">Sign in</button>
+                    <button type="submit" onClick={signIn} className="login__signInButton">Sign in</button>
                 </form>
 
                 <p>By signing-in you agree to the Amazon-clone Conditions of use & sale. Please see
                     our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                 </p>
 
-                <button className="login__registerButton">Create your Amazon account</button>
+                <button onClick={register} className="login__registerButton">Create your Amazon account</button>
             </div>
         </div>
     )
